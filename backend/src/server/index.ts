5aa6beb390edf436dc6b0aea94f95b632d7bfc2f
Lukasz Cholewka <ukasz.cholewka@gmail.com>
2021-07-21 14:46:05 +0200

    Fixed front, addes scripts to run project

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (_, res) => {
-  res.status(200).send("Ok !");
-})
+    res.status(200).send('Ok !');
+});
 
 app.listen(port, () => console.log(`Running on port ${port}`));
